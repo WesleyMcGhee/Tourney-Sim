02a476f390acf956ae4053fde0056901ed5cdb70
Wesley McGhee <wesmcghee6372@gmail.com>
2024-02-04 07:21:42 -0500

    added the languages now actually

---
+++ rust/target/debug/deps/rust-78bdc5c8f9e20662.d
@@ -0,0 +1,5 @@
+/home/wesleym/code-projs/idk-tourney/rust/target/debug/deps/librust-78bdc5c8f9e20662.rmeta: src/main.rs
+
+/home/wesleym/code-projs/idk-tourney/rust/target/debug/deps/rust-78bdc5c8f9e20662.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/deps/rust-c4c05338928f74fa.d
@@ -0,0 +1,5 @@
+/home/wesleym/code-projs/idk-tourney/rust/target/debug/deps/librust-c4c05338928f74fa.rmeta: src/main.rs
+
+/home/wesleym/code-projs/idk-tourney/rust/target/debug/deps/rust-c4c05338928f74fa.d: src/main.rs
+
+src/main.rs:
