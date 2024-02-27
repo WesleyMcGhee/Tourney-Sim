369e177bd2afdf278d65f9aaf295e184cd6b829c
Wesley McGhee <wesmcghee6372@gmail.com>
2024-02-26 20:54:47 -0600

    added python logic

--- python/main.py
+++ python/main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import random as rd
+
+class Tourney:
+    """A Tourney"""
+    def __init__(self):
+        players = self.players
+    
+    def CreateBraket():
+        
+
+class Player:
+    """A Player Class"""
+    def __init__(self):
+        wins = self.wins
